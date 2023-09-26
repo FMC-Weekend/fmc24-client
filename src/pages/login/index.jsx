feb0bd7e3fd9db7a8af4ea99120f139d1c31e77e
Shubham Kumar <shubham.kumar.min21@itbhu.ac.in>
2023-09-27 00:09:40 +0530

    err handle

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import Header from "../landingpage/Header"
-import Footer from "../landingpage/Footer"
-import Classes from './login.module.css';
-import GoogleLogin from 'react-google-login';
-const LogIn = () => {
-  return (
-    <>
-    <Header/>
- <div className={Classes.auth_section}>
-      <div className={Classes.top}></div>
-      
-      <div className={Classes.authenticateButton}>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-         
-          cookiePolicy="single_host_origin"
-        //   icon={false}
-          className={Classes.gButton}>
-          <p>Sign in with Google</p>
-        </GoogleLogin>
-         
-      </div>
-    </div>
-    <Footer/>
-    </>
-  )
-}
+// import React from 'react'
+// import Header from "../landingpage/Header"
+// import Footer from "../landingpage/Footer"
+// import Classes from './login.module.css';
+// import GoogleLogin from 'react-google-login';
+// const LogIn = () => {
+//     return (
+//         <>
+//             <Header />
+//             <div className={Classes.auth_section}>
+//                 <div className={Classes.top}></div>
 
-export default LogIn
+//                 <div className={Classes.authenticateButton}>
+//                     <GoogleLogin
+
+//                         className={Classes.gButton}>
+//                         <p>Sign in with Google</p>
+//                     </GoogleLogin>
+
+//                 </div>
+//             </div>
+//             <Footer />
+//         </>
+//     )
+// }
+
+// export default LogIn
